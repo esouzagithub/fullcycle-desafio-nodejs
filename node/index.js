738cebc8059b99f0ca9119c1c129d1f8369ffb4f
eduardo <eduardo@woowtech.com.br>
2024-03-19 15:57:06 -0300

    fix:desafio: corrige alguns detalhes do desafio

--- node/index.js
+++ node/index.js
@@ -3,45 +3,44 @@ const mysql = require('mysql');
 
 const app = express();
 
-const connection = mysql.createConnection({
-  host: 'mysqldb',
-  user: 'root',
-  password: 'root',
-  database: 'nodedb'
-});
+const config = {
+    host: 'mysqldb',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
 
+/*
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 });
 
-/*var sql = "DROP TABLE IF EXISTS people;";
+var sql = "DROP TABLE IF EXISTS people;";
 connection.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table deleted");
-});*/
-
-var sql = "CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))";
-connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
 });
-
-var sql = "INSERT INTO people (name) VALUES ('Eduardo Souza')";
-connection.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-});
+*/
 
 app.get('/', (req, res) => {
 
-    var orderinfo;   
-    var i;
-    var sql = "SELECT * FROM people;";
+    const connection = mysql.createConnection(config);
+    connection.query("CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255));", function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
     
-    connection.query(sql, (err, result) => {
+    connection.query("INSERT INTO people (name) VALUES ('Eduardo Souza');", function(err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+
+    let orderinfo;   
+    let i;   
+    connection.query("SELECT * FROM people;", (err, result) => {
         if(err) throw err;
-        var htmlStr = `<h1>Full Cycle Rocks!</h1><ul>`;
+        let htmlStr = `<h1>Full Cycle Rocks!</h1><ul>`;
         for(i = 0; i < result.length; i++){
             orderinfo = "Name: " + result[i].name + ".";
             htmlStr += `<li>${orderinfo}</li>`;
